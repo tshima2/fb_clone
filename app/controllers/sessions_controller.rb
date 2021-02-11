c60af2f50aa9551d958c1edfaff9672a2fa4d143
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-02-11 16:31:22 +0900

    create project, users controller/model, sessions controller/model

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン成功した場合
+      session[:user_id] = user.id
+      redirect_to user_path (user.id)
+    else
+      # ログイン失敗した場合
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_sessions_path
+  end
+end
